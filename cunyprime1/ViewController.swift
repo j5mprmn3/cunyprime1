34d6ff3a49bd736b8c8eaf6480bbf840fb87fa2e
Michael Rueda <michaelrueda389@gmail.com>
2015-12-13 23:14:36 -0500

    Initial Commit

---
+++ cunyprime1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cunyprime1
+//
+//  Created by Michael Rueda on 12/13/15.
+//  Copyright © 2015 Michael Rueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
